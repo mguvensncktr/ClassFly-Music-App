98381e33140211a24eaf0f3533e357d215261e8d
mguvensncktr <mguvensancaktar@gmail.com>
2022-02-15 15:47:51 +0300

    image assets

---
+++ constants/images.js
@@ -0,0 +1,25 @@
+const background = require('../assets/images/background.png');
+const ClassFly = require('../assets/images/ClassFly.png');
+const drum = require('../assets/images/drum.png');
+const guitar = require('../assets/images/guitar.png');
+const landing_bg = require('../assets/images/landing-bg.png');
+const mentor1 = require('../assets/images/mentor1.png');
+const mentor2 = require('../assets/images/mentor2.png');
+const mentor3 = require('../assets/images/mentor3.png');
+const piano_bg = require('../assets/images/piano-bg.png');
+const piano = require('../assets/images/piano.png');
+const saxophone = require('../assets/images/saxophone.png');
+
+export default {
+    background,
+    ClassFly,
+    drum,
+    guitar,
+    landing_bg,
+    mentor1,
+    mentor2,
+    mentor3,
+    piano_bg,
+    piano,
+    saxophone
+}

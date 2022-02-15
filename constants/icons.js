c5759cc0611e424d098cf280e25dc257550d70e4
mguvensncktr <mguvensancaktar@gmail.com>
2022-02-15 15:44:55 +0300

    icons added

---
+++ constants/icons.js
@@ -0,0 +1,21 @@
+export const arrow_right = require('../assets/icons/arrow-right.png');
+export const back = require('../assets/icons/back.png');
+export const home = require('../assets/icons/home.png');
+export const logo = require('../assets/icons/logo.png');
+export const menu = require('../assets/icons/menu.png');
+export const notification = require('../assets/icons/notification.png');
+export const pin = require('../assets/icons/pin.png');
+export const search = require('../assets/icons/search.png');
+export const user = require('../assets/icons/user.png');
+
+export default {
+    arrow_right,
+    back,
+    home,
+    logo,
+    menu,
+    notification,
+    pin,
+    search,
+    user
+}
